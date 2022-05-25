2d18947cea1d7ee79eb439883322f0e49fffefae
Ryan Acham <ryanacham@hotmail.com>
2022-05-24 23:39:01 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const quizCont = document.getElementById('options');
+const subButt = document.getElementById('submit');
+const resCont = document.getElementById('results');
+
+function quizCreate(){}
+function resultShow(){}
+
+quizCreate();
+
+subButt.addEventListener('click', resultShow);
